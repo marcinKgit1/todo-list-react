216627661d0a7f14bbf7e2fd206d7fb6152f50f2
Marcin Kozlowski <marcinkozlowski390@gmail.com>
2024-04-28 11:51:52 +0200

    task list without interactions yet

---
+++ src/Form/index.js
@@ -0,0 +1,15 @@
+import "./style.css";
+
+const Form = () => (
+    <form className="form">
+            <input
+              className="form__input"
+              placeholder="co jest do zrobienia?"
+            />
+            <button className="form__button">Dodaj nowe zadanie</button>
+          </form>
+);
+
+export default Form;
+
+

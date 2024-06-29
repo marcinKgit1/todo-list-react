b3e6aeda92de316f4325c97f49b9c143ade83bba
Marcin Kozlowski <marcinkozlowski390@gmail.com>
2024-06-29 12:13:54 +0200

    event interaction modified

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,11 +1,12 @@
-import {useState} from "react";
+import { useState } from "react";
 import "./style.css";
 
-const Form = ({addNewTask}) => {
+const Form = ({ addNewTask }) => {
   const [newTaskContent, setNewTaskContent] = useState("");
 
   const onFormSubmit = (event) => {
-    
+    event.preventDefault();
+
     const trimmedNewTaskContent = newTaskContent.trim();
 
     if (!trimmedNewTaskContent) {
@@ -18,17 +19,15 @@ const Form = ({addNewTask}) => {
 
   return (
     <form className="form" onSubmit={onFormSubmit}>
-            <input
-              value={newTaskContent}
-              className="form__input"
-              placeholder="co jest do zrobienia?"
-              onChange={({target}) => setNewTaskContent(target.value)}
-            />
-            <button className="form__button">Dodaj nowe zadanie</button>
-          </form>
-    );
+      <input
+        value={newTaskContent}
+        className="form__input"
+        placeholder="co jest do zrobienia?"
+        onChange={({ target }) => setNewTaskContent(target.value)}
+      />
+      <button className="form__button">Dodaj nowe zadanie</button>
+    </form>
+  );
 };
 
 export default Form;
-
-

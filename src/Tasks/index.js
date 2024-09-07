25e93b1e9ff4b4a6fbecc2581b3409bc210d6d9e
Marcin Kozlowski <marcinkozlowski390@gmail.com>
2024-09-07 12:20:04 +0200

    index.js modifeid with styled components

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,29 +1,18 @@
-import "./style.css";
-
-const Tasks = ({tasks, hideDone, removeTask, toggleTaskDone}) => (
-    <ul className="tasks">
-        {tasks.map(task => (
-        <li 
-        className={`tasks__item${task.done && hideDone ? " tasks__item--hidden" : ""}`} key={task.id}
-        >
-            <button 
-            className="tasks__button tasks__button--toggleDone "
-            onClick={() => toggleTaskDone(task.id)}
-            >
-        {task.done ? "✓" : ""}
-        </button>
-        <span className={`tasks__content${task.done ? " tasks__content--done" : ""}`}>
-        {task.content}
-        </span>
-        <button 
-            className="tasks__button tasks__button--remove"
-            onClick={() => removeTask(task.id)}
-            > 
-        🗑︎ 
-        </button>
-        </li>
-        ))}
-    </ul>
+import { List, Item, Content, Button } from "./styled";
+const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
+  <List>
+    {tasks.map((task) => (
+      <Item hidden={task.done && hideDone} key={task.id}>
+        <Button toggleDone onClick={() => toggleTaskDone(task.id)}>
+          {task.done ? "✓" : ""}
+        </Button>
+        <Content done={task.done}>{task.content}</Content>
+        <Button remove onClick={() => removeTask(task.id)}>
+          🗑︎
+        </Button>
+      </Item>
+    ))}
+  </List>
 );
 
 export default Tasks;
